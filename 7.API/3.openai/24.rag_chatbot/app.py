c03dcc6b40c21ffae66e43e655769ac5d987be7e
42kitrun <42kit.run@gmail.com>
2025-08-11 23:39:08 +0900

    20250811_class_notes

---
+++ 7.API/3.openai/24.rag_chatbot/app.py
@@ -0,0 +1,61 @@
+from flask import Flask, request, jsonify
+import os
+from vectorstore import initialize_vector_db, create_vector_db, delete_file_from_vstore
+from chatbot import initialize_llm, answer_question
+
+app = Flask(__name__, static_url_path="") # 굳이 static 안씀
+
+DATA_DIR = "./DATA"
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({"message": "파일이 없습니다."})
+    
+    file = request.files['file']
+    if file:
+        file_path = os.path.join(DATA_DIR, file.filename)
+        file.save(file_path)
+    
+    # 받아온 문서를 백터DB에 추가한다..
+    result = create_vector_db(file_path)
+    if result:
+        return jsonify({"message": "파일이 성공적으로 업로드 되었습니다."})        
+    else:
+         return jsonify({"message": "파일은 업로드 되었으나, DB가 정상적으로 생성되지 못했습니다."})    
+
+@app.route('/ask', methods=['POST'])
+def chatbot():
+    data = request.get_json()
+    question= data.get('question', '')
+    
+    answer = answer_question(question)
+    
+    return jsonify({"message": answer})
+
+@app.route('/files')
+def get_filelist():
+    files = [f for f in os.listdir(DATA_DIR)
+             if os.path.isfile(os.path.join(DATA_DIR, f))]
+    return jsonify({"files": files})
+
+@app.route('/files/<filename>', methods=['DELETE'])
+def delete_file(filename):
+    # 벡터DB에서 삭제
+    delete_file_from_vstore(filename)
+    
+    # 물리적으로 파일 삭제
+    path = os.path.join(DATA_DIR, filename)
+    if os.path.exists(path):
+        os.remove(path)
+        
+    return jsonify({"message": f"'{filename}' 을 삭제하였습니다."})
+
+if __name__== "__main__":
+    initialize_vector_db()
+    initialize_llm()
+    app.run(debug=True)
---
+++ 7.API/3.openai/24.rag_chatbot/vectorstore.py
@@ -0,0 +1,59 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+
+load_dotenv()
+
+# 필요한 글로벌 변수 선언
+VECTOR_DB = './chroma_db' # 쓸데없이 DB 커밋하지 않도록 .gitignore에 추가할것
+COLLECTION_NAME = "my-data"
+store = None
+
+def get_store():
+    return store
+
+def initialize_vector_db():
+    global store
+    # 이전 DB가 있었으면? 로딩..
+    if os.path.exists(VECTOR_DB) and os.listdir(VECTOR_DB):
+        store = Chroma(collection_name=COLLECTION_NAME, embedding_function=OpenAIEmbeddings(), persist_directory=VECTOR_DB)
+    else:
+        # 없으면? 디렉토리 생성
+        os.makedirs(VECTOR_DB, exist_ok=True)
+    return True
+    
+def create_vector_db(file_path):
+    global store
+    # 벡터 DB 생성...
+    # 1. 파일을 가져온다
+    documents = PyPDFLoader(file_path).load()
+    
+    for doc in documents:
+        doc.metadata['source'] = os.path.basename(file_path) # 경로 제외하고 파일명만 남기기
+        
+    # 2. 문서 분할한다
+    texts = CharacterTextSplitter(chunk_size=100, chunk_overlap=20).split_documents(documents)
+    print(texts)
+    
+    # 3. 임베딩 한다
+    embeddings = OpenAIEmbeddings()
+    if store:
+        store.add_documents(texts)
+    else:
+        store = Chroma.from_documents(
+            texts, embedding=embeddings, collection_name=COLLECTION_NAME, persist_directory=VECTOR_DB)
+    
+    print("벡터 DB가 정상적으로 생성되었습니다.")
+    return store
+
+def delete_file_from_vstore(filename):
+    # NoSQL 기반의 DB에서 자료 삭제하는것과 동일함 (예, mongodb)
+    store._collection.delete(where={"source": filename})
+    
+    # 백터DB가 persist 옵션이 켜져 있으면? 저장..
+    if hasattr(store, "persist"):
+        store.persist()
